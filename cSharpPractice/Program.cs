630412a7b348bb69e4b08b9b9e91a14b3bf66fd4
Takayuki Cho <taka.3365@gmail.com>
2019-01-26 16:50:11 +0900

    finish cSharpTutorial

--- cSharpPractice/Program.cs
+++ cSharpPractice/Program.cs
@@ -1,9 +1,105 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
 
 namespace cSharpPractice {
+
+    public class Vegetable
+    {
+        public Vegetable(string name) => Name = name;
+
+        public string Name { get; }
+
+        public override string ToString() => Name;
+
+    }
+
+    public enum Unit {
+        item,
+        kilogram,
+        gram,
+        dozen
+    };
+
+
     class Program {
+
         static void Main(string[] args){
-            System.Console.WriteLine("Hello World");
+            string command = args[0];
+
+            switch (command)
+            {
+                case "circle":
+                    circleArea();
+                    break;
+                case "cond":
+                    threeDivisible();
+                    break;
+                case "str":
+                    workingWithString();
+                    break;
+                case "list":
+                    workingWithList();
+                    break;
+                case "fibonacci":
+                    fibonacci();
+                    break;
+                case "class":
+                    workingWithClasses();
+                    break;
+                default:
+                    throw new Exception("this never happens");
+            }
         }
+
+        static void workingWithString(){
+            var veg = new Vegetable("eggPlant");
+            var date = DateTime.Now;
+            var price = 1.99m;
+            var unit = Unit.item;
+            Console.WriteLine($"On {date:d}, the price of {veg} was {price:C2} per {unit}.");
+        }
+
+        static void threeDivisible(){
+            for(int i = 1; i < 21; i++){
+                if(i % 3 == 0){
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+        static void workingWithClasses(){
+            var account = new BankAccount("<name>", 1000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+        }
+
+        static void circleArea(){
+            double radius = 2.50;
+            double area = radius * radius * Math.PI;
+            Console.WriteLine(area);
+        }
+
+        static void workingWithList() {
+            var names = new List<string> { "<name>", "Ana", "Felipe" };
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Hello {name.ToUpper()}!");
+            }
+        }
+
+        static void fibonacci(){
+            var ans = new List<int> {1,1};
+
+            while(ans.Count < 20 ){
+                ans.Add(ans[ans.Count - 1] + ans[ans.Count -2]);
+            }
+            foreach (var a in ans)
+            {
+                Console.WriteLine(a);
+            }
+
+
+
+        }
+
     }
 }
