44c376f07f0c1df4c8e1676aa9869a396a48e22d
Takayuki Cho <taka.3365@gmail.com>
2019-02-12 01:41:29 +0900

    adding unity practice

---
+++ unityPractices/Number Wizard/Assets/GameEngine.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameEngine : MonoBehaviour {
+
+	[SerializeField]int max;
+	[SerializeField]int min;
+	int guess;
+	[SerializeField]Text guessText;
+
+	// Use this for initialization
+	void Start () {
+		guess = (max + min) / 2;
+		guessText.text = guess.ToString();
+		// max = max + 1;
+	}
+
+	public void OnPressHigher(){
+		max = guess;
+		guess = (min + max) / 2;
+		guessText.text = guess.ToString();
+	}
+
+	public void OnPressLower(){
+		min = guess;
+		guess = (min + max) / 2;
+		guessText.text = guess.ToString();
+	}
+
+}
---
+++ unityPractices/Number Wizard/Assets/SceneLoader.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+	public void LoadNextScene() {
+		int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+		SceneManager.LoadScene(currentSceneIndex + 1);
+	}
+
+	public void BackToTopScene(){
+		int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+		Debug.Log(currentSceneIndex);
+		SceneManager.LoadScene(0);
+	}
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
